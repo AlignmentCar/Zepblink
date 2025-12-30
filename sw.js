9eab8796ee42a2828454d2f2bafd2dd0fc131148
AlignmentCar <sonney.ashu@gmail.com>
2025-12-30 11:10:29 +0530

    Initial commit

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Service Worker - Enables offline functionality
+const CACHE_NAME = 'inventory-v1';
+const urlsToCache = [
+  '/PurchaseOrderFixing/',
+  '/PurchaseOrderFixing/index.html'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
